02fdfb87fde98033027807d97ed7707d51d7c081
highvorz <highvorz@outlook.com>
2023-07-02 19:11:14 +0800

    raw version

--- pages/menu/menu.js
+++ pages/menu/menu.js
@@ -4,41 +4,88 @@ Page({
             id: 1,
             name: '奥尔良烤翅',
             price: 12,
+            tag: '人气热卖'
         },
+
+        toView: 'default',
 
         poster1: '/images/menu-poster1.jpg',
         poster2: '/images/menu-poster2.jpg',
         poster3: '/images/menu-poster3.jpg',
 
-
-
-        anchorlist:[
+        taglist:[
             {
                 tag: '人气热卖',
-                list:[
+                foodlist:[
                     {
                         food: 'hamburg',
                         price: 32,
                         img: '/images/food1.jpg',
+                        tag: '人气热卖'
                     },
                     {
                         food: 'hamburg',
                         price: 32,
                         img: '/images/food1.jpg',
+                        tag: '人气热卖'
                     }
                 ]
             },
             {
                 tag: '灵魂宵夜',
-                list:[
+                foodlist:[
                     {
                         food: 'else',
                         price: 32,
                         img: '/images/food2.jpg',
+                        tag: '灵魂宵夜'
                     }
                 ]
             },
-
+            {
+                tag: 'OK餐单人餐',
+                foodlist:[
+                    {
+                        food: 'else',
+                        price: 32,
+                        img: '/images/food2.jpg',
+                        tag: '灵魂宵夜'
+                    }
+                ]
+            },
+            {
+                tag: '多人餐',
+                foodlist:[
+                    {
+                        food: 'else',
+                        price: 32,
+                        img: '/images/food2.jpg',
+                        tag: '灵魂宵夜'
+                    }
+                ]
+            },
+            {
+                tag: '全鸡',
+                foodlist:[
+                    {
+                        food: 'else',
+                        price: 32,
+                        img: '/images/food2.jpg',
+                        tag: '灵魂宵夜'
+                    }
+                ]
+            },
+            {
+                tag: '炸鸡小食',
+                foodlist:[
+                    {
+                        food: 'else',
+                        price: 32,
+                        img: '/images/food2.jpg',
+                        tag: '灵魂宵夜'
+                    }
+                ]
+            },
         ],
 
         price: 0,
@@ -48,6 +95,12 @@ Page({
             food2: 4,
         }
 
+    },
+
+    scroll: function(e){
+        this.setData({
+            toView: 'view' + e.currentTarget.dataset.smile,
+       })
     },
 
     count: function(){
--- pages/user/user.js
+++ pages/user/user.js
@@ -1,13 +1,63 @@
-// pages/userLogin/user.js
-
 Page({
-
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        // realUrl: '',
-        openid: ''
+    data:{
+        list: [
+            {
+                img: '/images/icon-order-red.png',
+                content: '我的订单',
+                url: '../order/order',
+            },
+            {
+                img: '/images/icon-package-red.png',
+                content: '我的卡包',
+                url: '../package/package'
+            },
+            {
+                img: '/images/icon-vipCode-red.png',
+                content: '会员码',
+                func: 'to',
+                url: '../vipcode/vipcode'
+            },
+            {
+                img: '/images/icon-guide-red.png',
+                content: '在线客服',
+                func: 'to',
+                url: '../guide/guide'
+            },
+            {
+                img: '/images/icon-privacy-red.png',
+                content: '隐私政策',
+                func: 'to',
+                url: '../privacy/privacy'
+            },
+            {
+                img: '/images/icon-help-red.png',
+                content: '帮助中心',
+                func: 'to',
+                url: '../help/help'
+            },
+            {
+                img: '/images/icon-warning-red.png',
+                content: '如何自助点餐?',
+                func: 'to',
+                url: '../order/order'
+            },
+            {
+                img: '/images/icon-warning-red.png',
+                content: '会员工作日午餐时间说明',
+                func: 'to',
+                url: '../order/order'
+            },
+        ]
+    },
+    to: function(e){
+        wx.navigateTo({
+          url: e.currentTarget.dataset.smile.url,
+        })
     },
 
+    return: function(){
+        wx.navigateTo({
+          url: '../home/home',
+        })
+    }
 })
